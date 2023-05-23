b0c5cd3e5fd9c92d1978ae048e8cf285cfd78a12
Gizmo <cunyu@liuli.lol>
2023-05-23 13:47:01 +0800

    feat: wasm binding

--- build.zig
+++ build.zig
@@ -1,47 +1,52 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const lib = b.addSharedLibrary(.{
         .name = "totp-wasm-zig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+        .target = .{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+            .abi = .musl,
+        },
+        .optimize = .ReleaseSmall,
     });
+    lib.import_memory = true;
+    lib.rdynamic = true;
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
+    const run_opt = b.addSystemCommand(&.{
+        "wasm-opt",
+        "-Os",
+        "--strip-debug",
+        "--strip-producers",
+        "--zero-filled-memory",
+    });
+    const wasmopt_out = try std.fs.path.join(b.allocator, &.{
+        b.getInstallPath(.lib, ""),
+        "totp-wasm-zig.wasm",
+    });
+    defer b.allocator.free(wasmopt_out);
+    run_opt.addArtifactArg(lib);
+    run_opt.addArg("-o");
+    run_opt.addFileSourceArg(.{ .path = wasmopt_out });
+
+    const opt_step = b.step("opt", "optimize wasm file");
+    opt_step.dependOn(&run_opt.step);
+
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/otp.zig" },
         .target = target,
         .optimize = optimize,
     });
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,89 +1,21 @@
 const std = @import("std");
-const math = std.math;
-const testing = std.testing;
-const mem = std.mem;
-const Allocator = std.mem.Allocator;
-const HmacSha1 = std.crypto.auth.hmac.HmacSha1;
+const allocator = std.heap.wasm_allocator;
 
-pub fn hotp(key: []const u8, counter: u64, digit: u32) u32 {
-    var hmac: [HmacSha1.mac_length]u8 = undefined;
-    const counter_bytes = [8]u8{
-        @truncate(u8, counter >> 56),
-        @truncate(u8, counter >> 48),
-        @truncate(u8, counter >> 40),
-        @truncate(u8, counter >> 32),
-        @truncate(u8, counter >> 24),
-        @truncate(u8, counter >> 16),
-        @truncate(u8, counter >> 8),
-        @truncate(u8, counter),
-    };
+const otp = @import("./otp.zig");
 
-    HmacSha1.create(hmac[0..], counter_bytes[0..], key);
-
-    var offset = hmac[hmac.len - 1] & 0xf;
-    var bin_code = hmac[offset .. offset + 4];
-    var int_code = @as(u32, bin_code[3]) |
-        @as(u32, bin_code[2]) << 8 |
-        @as(u32, bin_code[1]) << 16 |
-        @as(u32, bin_code[0]) << 24 & 0x7FFFFFFF;
-
-    var code = int_code % (math.pow(u32, 10, digit));
-    return code;
+export fn hotp(key_ptr: [*]const u8, key_len: usize, counter: u64, digits: u32) u32 {
+    return otp.hotp(key_ptr[0..key_len], counter, digits);
 }
 
-test "hotp test" {
-    const key: []const u8 = "GM4VC2CQN5UGS33ZJJVWYUSFMQ4HOQJW";
-    const counter: u64 = 1662681600;
-    const digits: u32 = 6;
-    const code: u32 = 886679;
-
-    try testing.expectEqual(code, hotp(key, counter, digits));
+export fn totp(secret_ptr: [*]const u8, secret_len: usize, t: i64, digit: u32, period: u32) u32 {
+    return otp.totp(secret_ptr[0..secret_len], t, digit, period) catch return 0;
 }
 
-pub fn totp(secret: []const u8, t: i64, digit: u32, period: u32) !u32 {
-    const alloc = std.heap.page_allocator;
-    var counter = @divFloor(t, period);
-    var data = try base32_decode(alloc, secret);
-    defer alloc.free(data);
-    var code = hotp(data, @bitCast(u64, counter), digit);
-    return code;
+export fn malloc(size: usize) ?[*]const u8 {
+    const ret = allocator.alloc(u8, size + @sizeOf(usize)) catch return null;
+    return ret.ptr;
 }
 
-test "totp test" {
-    const secret: []const u8 = "GM4VC2CQN5UGS33ZJJVWYUSFMQ4HOQJW";
-    const t: i64 = 1662681600;
-    const digits: u32 = 6;
-    const period: u32 = 30;
-    const code: u32 = 473526;
-
-    try testing.expectEqual(code, try totp(secret, t, digits, period));
-}
-
-const RFC4648_ALPHABET: *const [32:0]u8 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-fn base32_decode(alloc: Allocator, input: []const u8) ![]u8 {
-    var unpad = input.len;
-    for (1..math.min(6, input.len) + 1) |i| {
-        if (input[input.len - i] != '=') break;
-        unpad -= 1;
-    }
-
-    const output_len = unpad * 5 / 8;
-
-    var output = try alloc.alloc(u8, output_len);
-    errdefer alloc.free(output);
-
-    for (0..input.len / 8) |i| {
-        var chunk = input[(i * 8)..((i + 1) * 8)];
-        var buf = [8]u8{ 0, 0, 0, 0, 0, 0, 0, 0 };
-        for (chunk, 0..) |c, ci|
-            buf[ci] = @intCast(u8, mem.indexOf(u8, RFC4648_ALPHABET, &[1]u8{c}).?);
-
-        output[i * 5 + 0] = (buf[0] << 3) | (buf[1] >> 2);
-        output[i * 5 + 1] = (buf[1] << 6) | (buf[2] << 1) | (buf[3] >> 4);
-        output[i * 5 + 2] = (buf[3] << 4) | (buf[4] >> 1);
-        output[i * 5 + 3] = (buf[4] << 7) | (buf[5] << 2) | (buf[6] >> 3);
-        output[i * 5 + 4] = (buf[6] << 5) | (buf[7]);
-    }
-    return output;
+export fn free(ptr: [*:0]u8) void {
+    allocator.free(std.mem.span(ptr));
 }
